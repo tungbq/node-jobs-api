88d77b4d97f74c5452a3a380cc6627ccd0fb5e56
Tung Bui <tungbq.info@gmail.com>
2021-11-22 22:54:41 +0700

    Create register route (wip)

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,22 @@
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+const { BadRequestError } = require('../errors');
+const bcrypt = require('bcryptjs');
+
 const register = async (req, res) => {
-	res.send('register user');
+	const { name, email, password } = req.body;
+	if (!name || !email || !password) {
+		throw new BadRequestError('Please provide name, email and password');
+	}
+
+	const salt = await bcrypt.genSalt(10);
+	const hashPassword = await bcrypt.hash(password, salt);
+
+	const newUser = { name, email, password: hashPassword };
+	console.log(newUser);
+
+	const user = await User.create(newUser);
+	res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = async (req, res) => {
