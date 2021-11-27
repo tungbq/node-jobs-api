62aacb416cd636c59d944f843590420a4e814872
Tung Bui <tungbq.info@gmail.com>
2021-11-27 11:53:08 +0700

    Add authentication middleware

---
+++ middleware/authentication.js
@@ -0,0 +1,25 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const { UnauthenticatedError } = require('../errors');
+const { StatusCodes } = require('http-status-codes');
+
+const auth = (req, res, next) => {
+	// check header
+	const authHeader = req.headers.authorization;
+
+	if (!authHeader || !authHeader.startsWith('Bearer')) {
+		throw new UnauthenticatedError('Authentication invalid!');
+	}
+
+	// Destructure token
+	const token = authHeader.split(' ')[1];
+
+	try {
+		var decoded = jwt.verify(token, process.env.JWT_SECRET);
+		// attach the user to the job routes
+		req.user = { userID: decoded.userID, name: decoded.name };
+		next();
+	} catch (error) {
+		res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error);
+	}
+};
